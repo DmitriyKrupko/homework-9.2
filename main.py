0c137d574c8014883377ef5033c96996da2dce2a
Makoto <dimonkrupko@yandex.ru>
2024-12-26 19:18:26 +0300

    edit/main_and_func

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-from func import args_list, three_lst, averege, min_max_arg
+from func import args_list
 x = int(input('Введите длинну списка: '))
-print(args_list(x))
-print(three_lst(args_list(x)))
-print('Среднее значение чисел: ', averege(three_list=(args_list(x)))) 
-min_max_arg(three_list=(args_list(x)))
+args_list(x)
